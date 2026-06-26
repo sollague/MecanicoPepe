e75e7897bfee6bf717b041299d74d1fe78624936
Sollague <asollague@gmail.com>
2026-06-26 17:13:34 -0500

    Delete Avance_6 directory

--- Avance_6/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ================= REDIRECCIÓN INICIAL =================
-
-// Cuando el usuario entra al proyecto, automáticamente lo envía al login
-header("Location: views/login_view.php");
-
-// Detiene la ejecución del script para evitar que siga cargando código innecesario
-exit();
